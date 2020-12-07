ba48f82aa84e4e22fab5b5ae13bdf0cc9377df75
lenart12 <39205323+Lenart12@users.noreply.github.com>
2020-12-07 18:36:54 +0100

    Added feedback form

---
+++ feedback.php
@@ -0,0 +1,27 @@
+<?php
+// Table for feedback
+/* CREATE TABLE `genetics`.`feedback` (
+    `id` INT NOT NULL AUTO_INCREMENT ,
+    `time` DATETIME NOT NULL ,
+    `text` TEXT NOT NULL ,
+    `contact` VARCHAR(200) NOT NULL ,
+    PRIMARY KEY (`id`))
+ENGINE = InnoDB; 
+*/
+
+// Save feedback in database
+if(isset($_GET['text']) and (count($_GET) == 1 or (count($_GET) == 2 && isset($_GET['contact'])))){
+    require_once("secret.php");
+    $conn = new mysqli(...$cred);
+    $insert = $conn->prepare('INSERT INTO feedback VALUES(NULL, CURRENT_TIMESTAMP(), ?, ?)');
+    $_GET['text'] = htmlspecialchars($_GET['text']);
+    $_GET['contact'] = htmlspecialchars($_GET['contact'] ?? '');
+    $insert->bind_param('ss', $_GET['text'], $_GET['contact']);
+    $insert->execute();
+    http_response_code(200); // HTTP: Success
+}
+else{
+    http_response_code(400); // HTTP: Bad request
+}
+die();
+?>
