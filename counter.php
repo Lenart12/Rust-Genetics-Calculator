79c8b0b54fd42ac2719440685355ee2c45f2e587
lenart12 <39205323+Lenart12@users.noreply.github.com>
2020-12-17 22:49:10 +0100

    Fixed bug creating a new session every refresh

--- counter.php
+++ counter.php
@@ -10,19 +10,23 @@ CREATE TABLE `genetics`.`pageviews` (
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB; 
 */
-require_once("secret.php");
 
-// Save IP, user-agent and request uri
-$ip = inet_pton( $_SERVER["REMOTE_ADDR"] );
-$ua = substr( htmlspecialchars( $_SERVER["HTTP_USER_AGENT"]  ), 0, 500);
-$rq = substr( htmlspecialchars( (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"  ), 0, 50);
-
-$conn = new mysqli(...$cred);
-
-$insert = $conn->prepare('INSERT INTO `pageviews` (`id`, `ip`, `useragent`, `request`, `time`) VALUES (NULL, ?, ?, ?, CURRENT_TIMESTAMP)');
-$insert->bind_param('sss', $ip, $ua, $rq);
-$insert->execute();
-
-// Set session id
 session_start();
-$_SESSION['user_session'] = $insert->insert_id;
+// Only track on session start
+if(!isset($_SESSION['user_session'])){
+    require_once("secret.php");
+    
+    // Save IP, user-agent and request uri
+    $ip = inet_pton( $_SERVER["REMOTE_ADDR"] );
+    $ua = substr( htmlspecialchars( $_SERVER["HTTP_USER_AGENT"]  ), 0, 500);
+    $rq = substr( htmlspecialchars( (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"  ), 0, 50);
+    
+    $conn = new mysqli(...$cred);
+    
+    $insert = $conn->prepare('INSERT INTO `pageviews` (`id`, `ip`, `useragent`, `request`, `time`) VALUES (NULL, ?, ?, ?, CURRENT_TIMESTAMP)');
+    $insert->bind_param('sss', $ip, $ua, $rq);
+    $insert->execute();
+    
+    // Set session id
+    $_SESSION['user_session'] = $insert->insert_id;
+}
