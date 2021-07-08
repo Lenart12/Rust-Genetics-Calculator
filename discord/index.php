03549825b2cbd529e44dc56dac2b282f623f8552
Lenart12 <koslenart@gmail.com>
2021-07-08 10:38:46 +0200

    Converted to pages

--- discord/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-// require_once("counter.php");
-header("location: https://discord.gg/k37gPJJkM2");
--- secret.template.php
+++
@@ -1,8 +0,0 @@
-<?php
-# Save copy of file as secret.php
-$cred = array(
-    /*"host"     =>*/ "localhost",
-    /*"username" =>*/ "AzureDiamond",
-    /*"password" =>*/ "hunter2",
-    /*"dbname"   =>*/ "genetics"
-);
