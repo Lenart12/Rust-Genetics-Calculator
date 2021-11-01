16f53287a15151fdd888b7305b3f26f2dc0f3154
lenart12 <39205323+Lenart12@users.noreply.github.com>
2021-11-01 22:24:12 +0100

    Added icon

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
     <meta name="twitter:image" content="http://wigaun.eu.org/genetics/rust.jpg">
 
     <title>Rust Genetics Calculator</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
